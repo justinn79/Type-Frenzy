f79e67cc0dd0aa24f69ffb188a18f4fcb6b9069f
justinn79 <justinlylemiraflor@gmail.com>
2025-03-04 15:58:18 -0500

    basic files

---
+++ code/main.py
@@ -0,0 +1,39 @@
+from settings import *
+
+class Game:
+    def __init__(self):
+        # setup
+        # pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Tipitype')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+        # sprites
+
+    def run(self):
+        while self.running:
+            # dt
+            dt = self.clock.tick() / 1000
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # update
+            self.all_sprites.update(dt)
+
+            # draw
+            self.display_surface.fill('black')
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
+
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ code/settings.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+from os.path import join 
+from os import walk
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720
+
+COLORS = {
+    'black': '#000000',
+    'red': '#ee1a0f',
+    'gray': 'gray',
+    'white': '#ffffff',
+    'blue': 'blue',
+    'yellow': 'yellow',
+    'purple': 'purple',
+}
