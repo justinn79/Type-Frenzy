e7d7b527414d6226d999ed885dde2f3e879e4ae6
justinn79 <justinlylemiraflor@gmail.com>
2025-03-12 21:12:34 -0400

    added a combo counter and pulsing effect to it

--- code/main.py
+++ code/main.py
@@ -29,10 +29,21 @@ class Game:
         self.number_of_queued_texts = 5
         self.list_of_queued_texts = []
         self.wordlist_index = 0 # this index is used to append the words from the wordlist to the list_of_queued_texts list
+        self.combo = 0
 
         # text shake variables
         self.shake = False
         self.shake_timer = 0
+
+        # combo text growing variables
+        self.pulse_grow = False
+        self.pulse_complete = False # we only want the text to pulse once, after that, then stop the pulse
+        self.combo_check = 0
+
+        self.growth_rate = 0.1
+        self.max_scale = 3
+        self.min_scale = 1
+        self.grow_scale = 1 # starts at grow_scale 1 to retain its original size
 
         # game settings
         self.text_rect_size_WIDTH = WINDOW_WIDTH // 3 # the queued and player_input string text rect WIDTH size
@@ -83,7 +94,64 @@ class Game:
         self.player_string_surf_y_original = WINDOW_HEIGHT - (WINDOW_HEIGHT // 6)
         self.player_string_surf_y = self.player_string_surf_y_original
 
-            
+    # --------------------------------- GAME LOGIC FUNCTIONS------------------------------------------------------------------------------------------#
+
+    def check_user_input(self):
+        # capping the length of the player_string so that the user cannot exceed a certain amount
+        if len(self.player_string) >= self.player_string_limit:
+            self.can_append_to_player_string = False
+        else:
+            self.can_append_to_player_string = True # this flag is used in the game loop to check whether or not the "player_string" can be added onto or not
+
+        # checks if the next word is equal to the player input string
+        if self.list_of_queued_texts[-1] == self.submit and self.submitted:
+            # print('correct')
+            self.list_of_queued_texts.pop(-1) # remove that word from the list of queued text
+            self.player_string = '' # resets the player string to get ready for the next word
+            self.typingtimer.reset_typing_timer() # resets the typing timer when the player string is correct
+            self.combo_count(increment=True)
+            self.submitted = False # after doing the CORRECT player input check to the queued text, we want to set this back to False so that this if statement doesnt continuously loop
+
+        elif self.list_of_queued_texts[-1] != self.submit and self.submitted:
+            print('WRONG')
+            self.shake_text(30) # shake the player text if it is wrong
+            self.healthbar.losing_hearts(1)
+            self.screenflash.screen_flash('red')
+            self.combo_count(reset=True)
+            self.submitted = False # after doing the INCORRECT player input check to the queued text, we want to set this back to False so that this if statement doesnt continuously loop
+
+        
+    def update_queued_word_list(self):
+        if len(self.list_of_queued_texts) < self.number_of_queued_texts:
+            self.list_of_queued_texts.insert(0, self.wordlist[self.wordlist_index])
+            self.wordlist_index += 1
+            # print(self.list_of_queued_texts)
+
+
+    # function to repeatedly check for user input
+    def typing_input(self, event):
+        
+        if event.type == pygame.KEYDOWN:
+
+            # handles the lower case letters
+            if event.unicode.lower() in self.letters and self.can_append_to_player_string: # if the key that was just pressed is a lower case letter that exists in self.letters, then add that to the player_string string variable
+                self.player_string += event.unicode.lower()
+
+            # handles the backspace
+            if event.key == pygame.K_BACKSPACE and len(self.player_string) > 0:
+                self.player_string = self.player_string[:-1] # this just gets rid of the last character in the "self.player_string" string variable
+
+            # handles the enter or space key
+            if event.key == pygame.K_RETURN or event.key == pygame.K_SPACE:
+                self.submit = self.player_string
+                self.submitted = True
+
+            # handles the escape key
+            if event.key == pygame.K_ESCAPE:
+                self.player_string = ""
+
+    # --------------------------------- GAME DRAWING FUNCTIONS -----------------------------------------------------------------------------------#
+
     def create_queued_text_rects(self, num_rects, rect_width, rect_height):
         rects = []
         spacing = 10  # space between each rectangle
@@ -170,22 +238,21 @@ class Game:
             text_rect.center = rect_center  # align the center of the text to the center of the rect
 
             # pygame.draw.circle(self.display_surface, COLORS['red'], (text_rect.centerx, text_rect.centery), 5) # using this red circle to check location of coordinate
-
             
             # draw the text
             # if the current queued text matches the last queued text in the list (which is the next text that the player has to input), then make the text pulsate
             if self.list_of_queued_texts[i] == self.list_of_queued_texts[-1]:
-                self.draw_pulsating_queued_text(self.list_of_queued_texts[i], (text_rect.centerx, text_rect.centery))
+                self.draw_pulsating_text(self.list_of_queued_texts[i], (text_rect.centerx, text_rect.centery))
             # otherwise, just blit the text normally
             else:
                 self.display_surface.blit(queued_text_surf, (text_x, text_y))
 
-    def draw_pulsating_queued_text(self, text, center_position):
+    def draw_pulsating_text(self, text, center_position):
 
         # initialize frame_count as an attribute of the function if it doesn't exist 
         if not hasattr(self, "frame_count"):
-            self.frame_count = 0  # initialize the counter
-            # i am using the 'hasattr' method to do a check everytime this 'draw_pulsating_queued_text' function is called to PREVENT 'frame_count' from being overwritten and set back to 0. i want to preserve the value from when it was called to the very end
+            self.frame_count = 0  # initialize the counter (frame count for the speed of the pulse)
+            # i am using the 'hasattr' method to do a check everytime this 'draw_pulsating_text' function is called to PREVENT 'frame_count' from being overwritten and set back to 0. i want to preserve the value from when it was called to the very end
 
         # using a sine wave for the pulsating text effect
         scale_factor = 1 + 0.1 * math.sin(self.frame_count * 2 * math.pi / 1000)  # adjust 1000 for speed
@@ -205,51 +272,6 @@ class Game:
         # incrementing the frame_count value to give it that pulsating effect
         self.frame_count += 20 # edit this value to decrease or increase the speed of the pulse
 
-
-    def check_user_input(self):
-        # capping the length of the player_string so that the user cannot exceed a certain amount
-        if len(self.player_string) >= self.player_string_limit:
-            self.can_append_to_player_string = False
-        else:
-            self.can_append_to_player_string = True # this flag is used in the game loop to check whether or not the "player_string" can be added onto or not
-
-        # checks if the next word is equal to the player input string
-        if self.list_of_queued_texts[-1] == self.submit and self.submitted:
-            # print('correct')
-            self.list_of_queued_texts.pop(-1) # remove that word from the list of queued text
-            self.player_string = '' # resets the player string to get ready for the next word
-            self.typingtimer.reset_typing_timer() # resets the typing timer when the player string is correct
-            self.submitted = False # after doing the CORRECT player input check to the queued text, we want to set this back to False so that this if statement doesnt continuously loop
-        elif self.list_of_queued_texts[-1] != self.submit and self.submitted:
-            print('WRONG')
-            self.shake_text(30) # shake the player text if it is wrong
-            self.healthbar.losing_hearts(1)
-            self.screenflash.screen_flash('red')
-            self.submitted = False # after doing the INCORRECT player input check to the queued text, we want to set this back to False so that this if statement doesnt continuously loop
-
-        
-    def update_queued_word_list(self):
-        if len(self.list_of_queued_texts) < self.number_of_queued_texts:
-            self.list_of_queued_texts.insert(0, self.wordlist[self.wordlist_index])
-            self.wordlist_index += 1
-            # print(self.list_of_queued_texts)
-
-    def shake_text(self, timer=None):
-        if timer:
-            self.shake = True
-            self.shake_timer = timer
-
-        if self.shake:
-            if self.shake_timer > 0: # if the shake timer is still above 0, decrement the value by 1 and apply the shake offset
-                self.shake_timer -= 1
-                self.player_string_surf_x -= random.randint(-2, 2) # shake offset position
-                self.player_string_surf_y -= random.randint(-1, 1) # shake offset position
-
-            if self.shake_timer <= 0: # once the shake timer hits 0, put the player text back in its original spot
-                self.player_string_surf_x = self.player_string_surf_x_original
-                self.player_string_surf_y = self.player_string_surf_y_original
-                self.shake = False # set self.shake to false so that this function wont be active again until the player inputs another wrong text (which gives this function another timer value to activate the function)
-
     def draw_player_input_text(self):
 
         self.player_string_surf = self.font.render(self.player_string, True, COLORS["white"])
@@ -268,9 +290,101 @@ class Game:
         pygame.draw.rect(self.display_surface, COLORS['blue'], bg_player_string_rect, 5, border_radius=10)
         self.display_surface.blit(self.player_string_surf, self.player_string_surf_rect)
 
+    def shake_text(self, timer=None):
+        if timer:
+            self.shake = True
+            self.shake_timer = timer
+
+        if self.shake:
+            if self.shake_timer > 0: # if the shake timer is still above 0, decrement the value by 1 and apply the shake offset
+                self.shake_timer -= 1
+                self.player_string_surf_x -= random.randint(-2, 2) # shake offset position
+                self.player_string_surf_y -= random.randint(-1, 1) # shake offset position
+
+            if self.shake_timer <= 0: # once the shake timer hits 0, put the player text back in its original spot
+                self.player_string_surf_x = self.player_string_surf_x_original
+                self.player_string_surf_y = self.player_string_surf_y_original
+                self.shake = False # set self.shake to false so that this function wont be active again until the player inputs another wrong text (which gives this function another timer value to activate the function)
+
+    def combo_count(self, increment=None, reset=None):
+
+        # if this function is called with reset=True, then reset the combo to 0 (when the player gets the input string wrong)
+        if reset:
+            self.combo = 0
+            
+
+        # creating the surface of the combo string
+        font_scale = 1
+        x_font = pygame.font.Font('fonts/Bungee-Regular.ttf', int(25 * font_scale))
+        combo_font = pygame.font.Font('fonts/Bungee-Regular.ttf', int(42 * font_scale))
+
+        x_string_surf = x_font.render("x", True, COLORS["white"])
+
+        combo_string_surf = combo_font.render(str(self.combo), True, COLORS["white"])
+
+        # combining the combo string and the "x" string together with a new surface (they both have different sizes so we are doing this approach)
+        combined_width = x_string_surf.get_width() + combo_string_surf.get_width() + 5  # add space between them
+        combined_height = max(x_string_surf.get_height(), combo_string_surf.get_height()) # getting the max height between the two to ensure that the height fits both
+        combined_surface = pygame.Surface((combined_width, combined_height), pygame.SRCALPHA)
+
+        #blitting both the "x" and combo string onto the new surface
+        combined_surface.blit(x_string_surf, (0, combined_height - x_string_surf.get_height() - 5)) # y value = combined_height - x_string_surf.get_height() - because we are blitting it from the top left so we want the full height of the combined height minus the height of the string were blitting (in this case, the "x" string) to put it at the bottom left of the surface
+        combined_surface.blit(combo_string_surf, (x_string_surf.get_width() + 5, 0)) # we want to blit this combo_string_surf at an x value equal to "x_string_surf.get_width" so it is blitted at the right of it. otherwise, it will overlap each other. we are also adding + 5 as an offset
+        
+        bottomleft_x = WINDOW_WIDTH // 24
+        bottomleft_y = WINDOW_HEIGHT - WINDOW_HEIGHT // 16
+
+        # --------------------------the code before this is what sets up the combo string before it takes on the grow effect----------------------------
+
+        # if this function is called with increment=True, then add onto the self.combo variable count by 1.
+        if increment:
+            self.combo += 1
+            # set this grow flag to true for the combo text to dramatically grow
+            self.pulse_grow = True
+            self.grow_scale = 1 # resettings the grow_scale to ensure that the new combo value will start the pulse grow from the beginning (resetting the pulse for a newly added combo value)
+            self.pulse_complete = False # setting the pulse_complete flag back to False to ensure that the pulse has not yet started for the current / newly incremented combo value
+        
+        # --------------------------GIVES THE COMBO STRING SURFACE THAT POP GROW EFFECT ------------------------------
+        if self.pulse_grow:
+            
+            # growing phase
+            if self.grow_scale < self.max_scale and not self.pulse_complete:
+                self.grow_scale += self.growth_rate # increment the scale by the growth rate
+
+            # once its at max scale, set pulse_complete to true
+            if self.grow_scale >= self.max_scale and not self.pulse_complete:
+                self.pulse_complete = True
+
+            # shrinking phase
+            if self.grow_scale > self.min_scale and self.pulse_complete:
+                self.grow_scale -= self.growth_rate # decrement the scale by the growth rate
+
+            # once it reaches the min scale and the pulse has been complete, stop the pulse and reset the variables
+            if self.grow_scale <= self.min_scale and self.pulse_complete:
+                self.grow_scale = 1
+                self.pulse_complete = False
+                self.pulse_grow = False # set the pulse_grow to False to exit the pulse_grow logic
+
+            # scaling the newly combined_surface with the new grow_scale
+            combined_surface = pygame.transform.scale(combined_surface, 
+                                            (int(combined_surface.get_width() * self.grow_scale),
+                                             int(combined_surface.get_height() * self.grow_scale)))
+
+
+        # -------- DRAWING THE COMBO STRING (we are drawing after the grow effect check, if the combo is not incremented, it will skip all the code above and go directly below to draw it on the screen----------------------
+        # getting the rect of the combined surface to be at a specified location (bottomleft_x, bottomleft_y)
+        # this rect will change depending on if the "combined_surface" is altered by the pulse_grow effect above (which is why we do the drawing last. just in case the pulse_grow effect does get applied).
+        # combined_rect will not get effected with the self.grow_scale above if it is not being applied because the code before the pulse_grow is always being iterated meaning combined surface is always being recalculated with the correct dimensions
+        combined_rect = combined_surface.get_frect(center=(bottomleft_x, bottomleft_y))
+        
+        self.display_surface.blit(combined_surface, combined_rect)
+            
+            
+    # --------------------------------- GAME DRAWING LOOP FUNCTION -----------------------------------------------------------------------------------#
+
     def draw_game(self):
     
-        # ------------- Queued text surface and the texts itself-------------------------------
+        # -------------queued text surface and the texts itself-------------------------------
         # drawing the queued text rects
         self.draw_queued_text_rects(self.queued_text_rects)
         # drawing the queued texts on top of the rects above
@@ -280,9 +394,12 @@ class Game:
         self.draw_player_input_text()
         self.shake_text() # if the player gets the input wrong, shake the text
 
+        self.combo_count()
+
         # ---------------------- Screen flash when player inputs wrong --------------------
         self.screenflash.screen_flash()
 
+    # --------------------------------- GAME LOGIC LOOP FUNCTION------------------------------------------------------------------------------------------#
 
     def game_logic(self):
         self.check_user_input()
@@ -293,27 +410,8 @@ class Game:
         self.healthbar.update()
         self.typingtimer.update()
     
-    # function to repeatedly check for user input
-    def typing_input(self, event):
-        
-        if event.type == pygame.KEYDOWN:
-
-            # handles the lower case letters
-            if event.unicode.lower() in self.letters and self.can_append_to_player_string: # if the key that was just pressed is a lower case letter that exists in self.letters, then add that to the player_string string variable
-                self.player_string += event.unicode.lower()
-
-            # handles the backspace
-            if event.key == pygame.K_BACKSPACE and len(self.player_string) > 0:
-                self.player_string = self.player_string[:-1] # this just gets rid of the last character in the "self.player_string" string variable
-
-            # handles the enter or space key
-            if event.key == pygame.K_RETURN or event.key == pygame.K_SPACE:
-                self.submit = self.player_string
-                self.submitted = True
-
-            # handles the escape key
-            if event.key == pygame.K_ESCAPE:
-                self.player_string = ""
+    
+    # --------------------------------- MAIN GAME LOOP -----------------------------------------------------#
 
     def run(self):
         while self.running:
