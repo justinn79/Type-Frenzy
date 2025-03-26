36c3a09d6f68af39e94700b7022709ae8247df67
justinn79 <justinlylemiraflor@gmail.com>
2025-03-26 13:51:57 -0400

    changed game icon file extension and path

--- code/game.py
+++ code/game.py
@@ -22,10 +22,10 @@ class Game:
         self.display_surface = display_surface
 
         # game title and game icon
-        self.game_icon = pygame.image.load('images/assets/game_icon.png').convert_alpha()
+        self.game_icon = pygame.image.load('images/game_icon/game_icon.ico').convert_alpha()
         pygame.display.set_icon(self.game_icon)
         pygame.display.set_caption('Type Frenzy')
-        
+
         self.clock = pygame.time.Clock()
         self.running = True
 
